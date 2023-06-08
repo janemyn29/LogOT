f1e72e322d805fd7246834a96288171a74bbaa49
Nguyễn Thị Trà My <tramygeo@gmail.com>
2023-06-08 14:01:28 +0700

    update db

--- src/WebUI/Services/FileManager/FileService.cs
+++ src/WebUI/Services/FileManager/FileService.cs
@@ -80,4 +80,58 @@ public class FileService : IFileService
             return null;
         }
     }
+
+    public string SavePdf(IFormFile imageFile)
+    {
+        try
+        {
+            var contentPath = this.environment.ContentRootPath;
+            // path = "c://projects/productminiapi/uploads" ,not exactly something like that
+            var path = Path.Combine(contentPath, "CV");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            // Check the allowed extenstions
+            var ext = Path.GetExtension(imageFile.FileName);
+            var allowedExtensions = new string[] { ".jpg", ".png", ".jpeg",".pdf" };
+            if (!allowedExtensions.Contains(ext))
+            {
+                string msg = string.Format("Only {0} extensions are allowed", string.Join(",", allowedExtensions));
+                throw new("Tải hình ảnh đã xảy ra lỗi!");
+            }
+            string uniqueString = Guid.NewGuid().ToString();
+            // we are trying to create a unique filename here
+            var newFileName = uniqueString + ext;
+            var fileWithPath = Path.Combine(path, newFileName);
+            var stream = new FileStream(fileWithPath, FileMode.Create);
+            imageFile.CopyTo(stream);
+            stream.Close();
+            return newFileName;
+        }
+        catch (Exception ex)
+        {
+            throw new("Tải hình ảnh đã xảy ra lỗi!");
+        }
+    }
+
+    public string CovertToBase64Pdf(string imageFileName)
+    {
+        try
+        {
+            var wwwPath = this.environment.WebRootPath;
+            var path = Path.Combine(wwwPath, "CV\\", imageFileName);
+            if (System.IO.File.Exists(path))
+            {
+                string base64 = File.ReadAllText(path);
+                return base64;
+            }
+            return null;
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+    }
 }
