b64cf639d7a0da73aefdf615e5875f2081bc24b6
Cà Phê Sữa Đá <113408584+vanw-is-me@users.noreply.github.com>
2023-06-18 01:29:00 +0700

    Crud Subsidize

---
+++ src/Application/Subsidize/Queries/GetSubsidize/SubsidizeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mentor_v1.Application.Common.Mappings;
+using mentor_v1.Domain.Entities;
+
+namespace mentor_v1.Application.Subsidize.Queries.GetSubsidize;
+
+public class SubsidizeViewModel : IMapFrom<Domain.Entities.Subsidize>
+{
+
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Amount { get; set; }
+
+    public IList<DepartmentAllowance> DepartmentAllowances { get; set; }
+
+}
